b36ae622fe03d25f3fb6b96d0825e4c8994529dc
thomas1922 <94869983+thomas1922@users.noreply.github.com>
2021-12-09 16:00:00 +0000

    Update Compra.java

--- Compra.java
+++ Compra.java
@@ -9,7 +9,15 @@ public class Compra implements Serializable {
     private Data data;
     private ArrayList<Produto> produtos;
 
-    public Compra(double preco, ArrayList<Produto> produtos,Data data) {
+    /**
+     * constructor de um compra que recebe como parametros o preço, a data da
+     * compra e a arrayList de produtos com os produtos comprados
+     *
+     * @param preco price
+     * @param produtos arrayList of objects with the type product
+     * @param data date of purchase
+     */
+    public Compra(double preco, ArrayList<Produto> produtos, Data data) {
         this.preco = preco;
         this.produtos = produtos;
         this.data = data;
@@ -19,29 +27,28 @@ public class Compra implements Serializable {
         return preco;
     }
 
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
     public Data getData() {
         return data;
-    }
-
-    public void setData(Data data) {
-        this.data = data;
     }
 
     public ArrayList<Produto> getProdutos() {
         return produtos;
     }
 
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
     public void setProdutos(ArrayList<Produto> produtos) {
         this.produtos = produtos;
     }
-    
-   
+
     @Override
-    public String toString(){
-        return "Data: "+getData()+"  Preço: "+getPreco()+" "+getProdutos();
+    public String toString() {
+        return "Data: " + getData() + "  Preço: " + getPreco() + " " + getProdutos();
     }
 }
