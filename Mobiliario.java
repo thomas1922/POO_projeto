3da4c3a706e2095695813550be578d47feb9b604
thomas1922 <94869983+thomas1922@users.noreply.github.com>
2021-12-09 16:00:53 +0000

    Update Mobiliario.java

--- Mobiliario.java
+++ Mobiliario.java
@@ -2,11 +2,23 @@ package supermercado;
 
 import java.io.Serializable;
 
-public class ProdutoMobiliario extends Produto implements Serializable{
+public class ProdutoMobiliario extends Produto implements Serializable {
+
     private int peso;
     private int dimensao;
-    
-    public ProdutoMobiliario(int identificador, String nome, Double precoUnitario, int stock,int peso,int dimensao) {
+
+    /**
+     * method that calls the constructor of the super class, receiving its
+     * arguments, plus the weight and size
+     *
+     * @param identificador Identifier
+     * @param nome Name
+     * @param precoUnitario Price
+     * @param stock Stock
+     * @param peso weight
+     * @param dimensao size
+     */
+    public ProdutoMobiliario(int identificador, String nome, Double precoUnitario, int stock, int peso, int dimensao) {
         super(identificador, nome, precoUnitario, stock);
         this.peso = peso;
         this.dimensao = dimensao;
@@ -16,20 +28,20 @@ public class ProdutoMobiliario extends Produto implements Serializable{
         return peso;
     }
 
-    public void setPeso(int peso) {
-        this.peso = peso;
-    }
-
     public int getDimensao() {
         return dimensao;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
     }
 
     public void setDimensao(int dimensao) {
         this.dimensao = dimensao;
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         return getNome();
     }
 }
