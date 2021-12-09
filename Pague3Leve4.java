c164f13d510b39843e61aefd0ac32f58e73ac212
thomas1922 <94869983+thomas1922@users.noreply.github.com>
2021-12-09 16:01:16 +0000

    Update Pague3Leve4.java

--- Pague3Leve4.java
+++ Pague3Leve4.java
@@ -1,20 +1,29 @@
-
 package supermercado;
 
 public class Pague3Leve4 extends Promocao {
-    public Pague3Leve4(String nomeProduto,Data dataInicio, Data dataFim, Data dataHoje){
-        super(nomeProduto,dataInicio,dataFim,dataHoje);
+
+    public Pague3Leve4(String nomeProduto, Data dataInicio, Data dataFim, Data dataHoje) {
+        super(nomeProduto, dataInicio, dataFim, dataHoje);
+    }
+
+    /**
+     * method that returns the discount value of the price of a product
+     *
+     * @param quantidade amount
+     * @param preçoUnitario price
+     * @return discount
+     */
+    @Override
+    public Double promocao(int quantidade, Double preçoUnitario) {
+        /*Double preçoTotal = quantidade * preçoUnitario,*/
+        Double desconto;
+        int produtosGratuitos = (int) (quantidade / 4);
+        desconto = produtosGratuitos * preçoUnitario;
+        return desconto;
     }
 
     @Override
-    public Double promoçao(int quantidade, Double preçoUnitario) {
-        Double preçoTotal=quantidade*preçoUnitario,desconto;
-        int produtosGratuitos=(int)(quantidade/4);
-        desconto=produtosGratuitos*preçoUnitario;
-        return desconto;
-    }
-    @Override
-    public String toString(){
-        return " ";
+    public String toString() {
+        return "Tipo:Pague3Leve4 Nome: " + getNomeProduto();
     }
 }
