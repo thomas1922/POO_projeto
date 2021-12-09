ffb93225641a8c0573d9ab77b5ccdf3db331e02d
thomas1922 <94869983+thomas1922@users.noreply.github.com>
2021-12-09 17:55:29 +0000

    Update ProdutoLimpeza.java

--- ProdutoLimpeza.java
+++ ProdutoLimpeza.java
@@ -1,6 +1,3 @@
-package supermercado;
-
-import java.io.Serializable;
 
 public class ProdutoLimpeza extends Produto implements Serializable {
 
@@ -10,10 +7,10 @@ public class ProdutoLimpeza extends Produto implements Serializable {
      * method that calls the constructor of the super class, receiving its
      * arguments, plus the degree of toxicity
      *
-     * @param identificador Identifier
-     * @param nome Name
-     * @param precoUnitario Price
-     * @param stock Stock
+     * @param identificador identifier
+     * @param nome name
+     * @param precoUnitario price
+     * @param stock stock
      * @param grauToxicidade degree of toxicity
      */
     public ProdutoLimpeza(int identificador, String nome, Double precoUnitario, int stock, int grauToxicidade) {
