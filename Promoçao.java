e05f3e5271ce3af5f47dcb622272c76df467a740
thomas1922 <94869983+thomas1922@users.noreply.github.com>
2021-12-09 16:04:01 +0000

    Update Promoçao.java

--- Promoçao.java
+++ Promoçao.java
@@ -3,18 +3,30 @@ package supermercado;
 import java.io.Serializable;
 
 public abstract class Promocao implements Serializable {
+
     private String nomeProduto;
     private Data dataInicio;
     private Data dataFim;
     private int ativa;
 
-    public Promocao(String nomeProduto,Data dataInicio, Data dataFim, Data dataHoje) {
-        this.nomeProduto=nomeProduto;
+    /**
+     * constructor of a promotion that receives as parameters the name of the
+     * product, the date of beginning of the promotion, the date of end and the
+     * current date. The promotion is activated or not using the method
+     * ativaPromoção that receives as parameter the current date
+     *
+     * @param nomeProduto Name
+     * @param dataInicio initial date
+     * @param dataFim final date
+     * @param dataHoje current date
+     */
+    public Promocao(String nomeProduto, Data dataInicio, Data dataFim, Data dataHoje) {
+        this.nomeProduto = nomeProduto;
         this.dataInicio = dataInicio;
         this.dataFim = dataFim;
-        this.ativa=ativaPromocao(dataHoje);
+        this.ativa = ativaPromocao(dataHoje);
     }
-    
+
     public Data getDataInicio() {
         return dataInicio;
     }
@@ -46,19 +58,35 @@ public abstract class Promocao implements Serializable {
     public String getNomeProduto() {
         return nomeProduto;
     }
-    
-    
-    public abstract Double promoçao(int quantidade, Double preçoUnitario);
-    
-    public int ativaPromocao(Data dataHoje){
+
+    /**
+     * abstract method
+     *
+     * @param quantidade amount
+     * @param preçoUnitario price
+     * @return discount
+     */
+    public abstract Double promocao(int quantidade, Double preçoUnitario);
+
+    /**
+     * method that uses the comparaDatas method which takes as parameters the
+     * promotion start date, promotion end date and current date and returns 0
+     * or 1 if the current date is between the two previously mentioned. If it
+     * returns 1 the promotion is active, if it returns 0 it's inactive
+     *
+     * @param dataHoje current date
+     * @return 1 or 0
+     */
+    public int ativaPromocao(Data dataHoje) {
         int ativador;
         Data data = new Data();
-        ativador= data.comparaDatas(dataInicio, dataFim, dataHoje);
+        ativador = data.comparaDatas(dataInicio, dataFim, dataHoje);
         return ativador;
     }
-    
+
     @Override
-    public String toString(){
-        return " ";}
-     
+    public String toString() {
+        return " ";
+    }
+
 }
