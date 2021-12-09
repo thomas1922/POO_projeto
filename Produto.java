b0f7c7ac5854beb8fb7ad2e3c9481ef1ee70f982
thomas1922 <94869983+thomas1922@users.noreply.github.com>
2021-12-09 16:01:59 +0000

    Update Produto.java

--- Produto.java
+++ Produto.java
@@ -2,60 +2,71 @@ package supermercado;
 
 import java.io.Serializable;
 
-public class  Produto implements Serializable {
-   private int identificador;
-   private String nome;
-   private Double precoUnitario;
-   private int stock;
-   
-     
-     public Produto(int identificador, String nome, Double precoUnitario, int stock) {
+public class Produto implements Serializable {
+
+    private int identificador;
+    private String nome;
+    private Double precoUnitario;
+    private int stock;
+
+    /**
+     * constructor which takes as parameters an identifier, a name, the price
+     * and the existing stock
+     *
+     * @param identificador Identifier
+     * @param nome Name
+     * @param precoUnitario price
+     * @param stock stock
+     */
+    public Produto(int identificador, String nome, Double precoUnitario, int stock) {
         this.identificador = identificador;
         this.nome = nome;
         this.precoUnitario = precoUnitario;
         this.stock = stock;
     }
-     
-    public Produto(){
-        
-    }
-   
-    public int getIdentificador() {
-        return identificador;
+
+    /**
+     * Empty constructor
+     */
+    public Produto() {
+
     }
 
-    public void setIdentificador(int identificador) {
-        this.identificador = identificador;
+    public int getIdentificador() {
+        return identificador;
     }
 
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public Double getPrecoUnitario() {
         return precoUnitario;
-    }
-
-    public void setPrecoUnitario(Double precoUnitario) {
-        this.precoUnitario = precoUnitario;
     }
 
     public int getStock() {
         return stock;
     }
 
+    public void setIdentificador(int identificador) {
+        this.identificador = identificador;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPrecoUnitario(Double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
     public void setStock(int stock) {
         this.stock = stock;
     }
-  
-   @Override
-  public String toString(){
-      return getNome()+" ";
-  }
-   
-   
+
+    @Override
+    public String toString() {
+        return getNome() + " ";
+    }
+
 }
