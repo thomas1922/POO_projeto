b3c7b79ec6e445dd8e87603343bdb05dd7211607
thomas1922 <94869983+thomas1922@users.noreply.github.com>
2021-12-09 16:02:27 +0000

    Update ProdutoAlimentar.java

--- ProdutoAlimentar.java
+++ ProdutoAlimentar.java
@@ -2,11 +2,23 @@ package supermercado;
 
 import java.io.Serializable;
 
-public class ProdutoAlimentar extends Produto implements Serializable{
+public class ProdutoAlimentar extends Produto implements Serializable {
+
     private int numCalorias;
     private int percentGordura;
-    
-    public ProdutoAlimentar(int identificador, String nome, Double precoUnitario, int stock,int numCalorias,int percentGordura) {
+
+    /**
+     * method that calls the constructor of the super class, receiving its
+     * arguments, plus the number of calories and fat percentage
+     *
+     * @param identificador Identifier
+     * @param nome Name
+     * @param precoUnitario Price
+     * @param stock Stock
+     * @param numCalorias number of calories
+     * @param percentGordura fat percentage
+     */
+    public ProdutoAlimentar(int identificador, String nome, Double precoUnitario, int stock, int numCalorias, int percentGordura) {
         super(identificador, nome, precoUnitario, stock);
         this.numCalorias = numCalorias;
         this.percentGordura = percentGordura;
@@ -16,12 +28,12 @@ public class ProdutoAlimentar extends Produto implements Serializable{
         return numCalorias;
     }
 
-    public void setNumCalorias(int numCalorias) {
-        this.numCalorias = numCalorias;
-    }
-
     public int getPercentGordura() {
         return percentGordura;
+    }
+
+    public void setNumCalorias(int numCalorias) {
+        this.numCalorias = numCalorias;
     }
 
     public void setPercentGordura(int percentGordura) {
@@ -29,8 +41,8 @@ public class ProdutoAlimentar extends Produto implements Serializable{
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return getNome();
     }
-    
+
 }
